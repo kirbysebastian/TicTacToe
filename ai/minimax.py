69d56f34000c1c59c7c072fc5bc39b979949577c
kirbysebastian <kirbypaolo.sebastian@gmail.com>
2020-01-12 10:45:25 +0800

    minimax description

--- ai/minimax.py
+++ ai/minimax.py
@@ -1,4 +1,14 @@
 # STILL TODO
 
+'''
+Board Position Representation
+ 1 | 2 | 3
+---+---+---
+ 4 | 5 | 6 
+---+---+---
+ 7 | 8 | 9
+'''
+
+# GOAL: Returns the best position available in board
 def minimax(board, depth, maximizingPlayer):
 	pass
